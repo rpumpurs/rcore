afb43fe10c36196fb5bca8bea6ac9b3fee4bd742
Rudolfs <rudolfs.pumpurs@gmail.com>
2020-05-06 22:00:24 +0300

    Add mysql and pma and users table

---
+++ etc/pma/config.user.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$cfg['MaxNavigationItems'] = 2000;
+$cfg['LoginCookieValidity'] = 3600 * 24;
---
+++ migrations/2_create_users_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `rcore`.`users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(45) NULL,
+`password` VARCHAR(45) NULL,
+PRIMARY KEY (`id`));
